0e686c7d20d3809e75f2cd6bf1a75229003595b9
Weixin Zhou <zwx@rock-chips.com>
2019-12-24 18:03:38 +0800

    add npu powerctrl for control pin combine board
    Change-Id: Ifbd0bc2b0f43c7b9d270ab5ce6e932fc354721c8
    Signed-off-by: Weixin Zhou <zwx@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -8,7 +8,11 @@ LOCAL_CPPFLAGS += -D_FILE_OFFSET_BITS=64 -D_LARGE_FILE -fexceptions -Wno-non-vir
 LOCAL_CFLAGS += -Wno-unused-function 
 LOCAL_LDLIBS = -Wl,-Bstatic -Wl,-Bdynamic -Wl,-Bstatic
 LOCAL_SRC_FILES := main.cpp
+ifeq ($(BOARD_NPU_POWER_COMBINE),true)
+LOCAL_SRC_FILES += npu_powerctrl_combine.c
+else
 LOCAL_SRC_FILES += npu_powerctrl.c
+endif
 
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TARGET_ARCH := arm64
